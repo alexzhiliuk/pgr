fb5457108970870d62f5ae7eb63ff839bb8bdd6e
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-02-23 14:41:58 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from './modules/functions.js';
+import * as burger from './modules/burger.js';
+
+
+flsFunctions.isWebp()
+
+import Swiper, {Autoplay, Navigation, Pagination} from 'swiper';
+Swiper.use([Autoplay, Navigation, Pagination]);
+
+
