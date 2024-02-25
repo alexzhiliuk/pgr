18c7f14ec2c72815b254dfc023d87dc6d6038c7b
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-02-25 17:18:09 +0300

    consultation form: form

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import * as burger from './modules/burger.js';
 import * as menuAccordion from './modules/menuAccordion.js';
 import * as hero from './modules/hero.js';
 import * as companiesSlider from './modules/companiesSlider.js';
+import * as phoneMask from './modules/phoneMask.js';
 
 
 flsFunctions.isWebp()
