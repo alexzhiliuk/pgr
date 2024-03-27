ef86176d2ff37cd1a581a65c64afd22cd65d1f07
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-03-27 20:22:54 +0300

    horizontal tabs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -5,3 +5,17 @@ $(".tabs__item").click(function() {
     $(`.tabs__item[data-tab-id=${id}]`).addClass("tabs__item_active")
     $(`.tabs__content[data-tab-id=${id}]`).addClass("tabs__content_active")
 })
+
+$(".horizontal-tabs__item").click(function() {
+    let id = $(this).attr("data-tab-id")
+    $(".horizontal-tabs__item_active").removeClass("horizontal-tabs__item_active")
+    $(".horizontal-tabs__content_active").removeClass("horizontal-tabs__content_active")
+    $(`.horizontal-tabs__item[data-tab-id=${id}]`).addClass("horizontal-tabs__item_active")
+    $(`.horizontal-tabs__content[data-tab-id=${id}]`).addClass("horizontal-tabs__content_active")
+})
+
+$(".horizontal-tabs__content-btn").click(function(e) {
+    e.preventDefault()
+    $(this).parent().find(".horizontal-tabs__content-text").toggleClass("horizontal-tabs__content-text_opened")
+    $(this).toggleClass("horizontal-tabs__content-btn_opened")
+})
