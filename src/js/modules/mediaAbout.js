c698eab6f6e3fa1254a3ace03e28512f49793ef8
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-11-01 19:10:20 +0300

    slider with form

--- src/js/modules/mediaAbout.js
+++ src/js/modules/mediaAbout.js
@@ -3,7 +3,7 @@ Swiper.use([Autoplay, Navigation, Pagination]);
 
 
 
-const swiperHero = new Swiper('.article-slider__container', {
+const swiperArticles = new Swiper('.article-slider__container:not(.article-slider__container_with-form)', {
     slidesPerView: 1.15,
     spaceBetween: 24,
     speed: 1500,
@@ -34,3 +34,31 @@ const swiperHero = new Swiper('.article-slider__container', {
 
 
 
+const swiperArticlesWithForm = new Swiper('.article-slider__container_with-form', {
+    slidesPerView: 1.15,
+    spaceBetween: 24,
+    speed: 1500,
+    freeMode: true,
+
+    navigation: {
+        nextEl: '.article-slider__btn_next',
+        prevEl: '.article-slider__btn_prev',
+    },
+
+    breakpoints: {
+        1366: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3.2,
+        },
+        480: {
+            slidesPerView: 2.2,
+        },
+    },
+
+    
+});
